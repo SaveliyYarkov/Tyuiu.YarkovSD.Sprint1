e84121ab3077bfdbe770e340ba95eb0259e2cd2f
BorneO <savelis1908@gmail.com>
2025-09-30 12:29:48 +0300

    Task0

---
+++ Tyuiu.YarkovSD.Sprint0.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.YarkovSD.Sprint1.Task0.V19.Lib;
+
+namespace Tyuiu.YarkovSD.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
