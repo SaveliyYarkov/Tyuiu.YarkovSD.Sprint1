b300e451e2aff29635a8f912a924b7b21a71d4a7
BorneO <savelis1908@gmail.com>
2025-09-30 13:39:01 +0300

    Task0

--- Tyuiu.YarkovSD.Sprint1.Task0.V19.Lib/DataService.cs
+++ Tyuiu.YarkovSD.Sprint1.Task0.V19.Lib/DataService.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using tyuiu.cources.programming.interfaces.Sprint1;
 
-namespace Tyuiu.YarkovSD.Sprint1.Task0.V0.Lib
+namespace Tyuiu.YarkovSD.Sprint1.Task0.V19.Lib
 {
     public class DataService : ISprint1Task0V19
     {
--- Tyuiu.YarkovSD.Sprint1.Task0.V19/Program.cs
+++ Tyuiu.YarkovSD.Sprint1.Task0.V19/Program.cs
@@ -1,5 +1,10 @@
-﻿using Tyuiu.YarkovSD.Sprint1.Task0.V19.Lib;
-namespace Tyiui.YarkovSD.Sprint1.Task0.V19
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.YarkovSD.Sprint1.Task0.V19.Lib;
+namespace Tyiui.YarkovSD.Sprint1.Task0.V19;
 internal class Program
 {
     private static void Main(string[] args)
@@ -11,23 +16,23 @@ internal class Program
         Console.WriteLine("* Спринт #1                                                               *");
         Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
         Console.WriteLine("* Задание #0                                                              *");
-        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Вариант #0                                                              *");
         Console.WriteLine("* Выполнил: Ярков Савелий Денисович | РППб-25-1                           *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Написать программу, которая вычисляет выражение 4 / 2 * 5 / (3 + 2) * 5 *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 4/2*5/(3+2)*5           *");
         Console.WriteLine("* и печатает результат на экране.                                         *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
-        Console.WriteLine("* 4 / 2 * 5 / (3 + 2) * 5                                                 *");
+        Console.WriteLine("* 4/2*5/(3+2)*5                                                           *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
 
         Console.WriteLine(ds.Calculate());
 
-        Console.ReadLine();
+        Console.ReadKey();
     }
 }
