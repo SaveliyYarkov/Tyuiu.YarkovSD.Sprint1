a6cb51bee73aa333c3e6603f3e415cbfce0d32a4
BorneO <savelis1908@gmail.com>
2025-09-30 12:00:05 +0300

    Добавьте файлы проекта.

---
+++ Tyuiu.YarkovSD.Sprint0.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint0.Task1.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
