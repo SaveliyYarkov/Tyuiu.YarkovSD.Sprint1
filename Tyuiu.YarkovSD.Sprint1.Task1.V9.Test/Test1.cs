1e457bb454a05e7005046cc651f23e6f00520f5e
BorneO <savelis1908@gmail.com>
2025-09-30 14:56:40 +0300

    Task1

---
+++ Tyuiu.YarkovSD.Sprint1.Task1.V9.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint1.Task1.V9.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
