c23c7aa6e298dfb52800e6556f8e8aec93871b97
BorneO <savelis1908@gmail.com>
2025-09-30 16:07:39 +0300

    Task2

--- Tyuiu.YarkovSD.Sprint1.Task2.V16/Program.cs
+++ Tyuiu.YarkovSD.Sprint1.Task2.V16/Program.cs
@@ -26,7 +26,7 @@ namespace Tyuiu.YarkovSD.Sprint1.Task2.V0
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("Периметр круга = " + Math.Round(ds.CalculatePerimetrCircle(x), 3));
+            Console.WriteLine("Периметр круга = " + (string.Format("{0:3}", ds.CalculatePerimetrCircle(x))));
 
             Console.ReadLine();
         }
