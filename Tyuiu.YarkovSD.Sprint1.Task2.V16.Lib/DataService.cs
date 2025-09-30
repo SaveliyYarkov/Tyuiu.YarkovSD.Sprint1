bf4f9e93efbe92164d054b0c184186e398ccc1e3
BorneO <savelis1908@gmail.com>
2025-09-30 15:31:40 +0300

    Task2

---
+++ Tyuiu.YarkovSD.Sprint1.Task2.V16.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.YarkovSD.Sprint1.Task2.V0.Lib
+{
+    public class DataService : ISprint1Task2V16
+    {
+        public int Sqr(int value)
+        {
+            return value * value;
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint1.Task2.V16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.YarkovSD.Sprint1.Task2.V0.Lib;
+
+namespace Tyuiu.YarkovSD.Sprint1.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Квадрат числа X = " + ds.Sqr(x));
+
+            Console.ReadLine();
+        }
+    }
+}
