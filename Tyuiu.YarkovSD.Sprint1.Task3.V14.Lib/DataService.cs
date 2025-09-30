926726b26e250f1e55139c355dde9e3ce68424bd
BorneO <savelis1908@gmail.com>
2025-09-30 16:39:15 +0300

    Task3

---
+++ Tyuiu.YarkovSD.Sprint1.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.YarkovSD.Sprint1.Task3.V13.Lib
+{
+    public class DataService : ISprint1Task3V14
+    {
+        public double ReverseNumber(int number)
+        {
+            int Number1 = number / 100;
+            int Number2 = (number / 10) % 10;
+            int Number3 = number % 10;
+            return(Number3 * 100 + Number2 * 10 + Number1);
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint1.Task3.V14/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.YarkovSD.Sprint1.Task3.V13.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.ReverseNumber(a));
+
+Console.ReadKey();
