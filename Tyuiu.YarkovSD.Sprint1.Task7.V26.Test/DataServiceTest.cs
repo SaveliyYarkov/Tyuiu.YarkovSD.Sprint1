4044f9c53952b06ca8c1dcc8f2266cd39199d7ab
BorneO <savelis1908@gmail.com>
2025-10-02 16:08:22 +0300

    Task7

---
+++ Tyuiu.YarkovSD.Sprint1.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint1.Task7.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint1.Task7.V26/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.YarkovSD.Sprint1.Task7.V26.Lib;
+
+namespace Tyuiu.YarkovSD.Sprint1.Task7.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("    sin(x^2) + y        xy - 12                                            ");
+            Console.WriteLine("z = ------------   -   ---------                                           ");
+            Console.WriteLine("       y + 1            34 + x^2                                           ");
+            double x, y, z;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
