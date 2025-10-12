6cdc4af1343914ed1054b43ae57c65b61b0df254
BorneO <savelis1908@gmail.com>
2025-10-12 13:54:38 +0300

    Task7

--- Tyuiu.YarkovSD.Sprint1.Task7.V26.Test/DataServiceTest.cs
+++ Tyuiu.YarkovSD.Sprint1.Task7.V26.Test/DataServiceTest.cs
@@ -1,11 +1,22 @@
-﻿namespace Tyuiu.YarkovSD.Sprint1.Task7.V26.Test
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.YarkovSD.Sprint1.Task7.V26.Lib;
+
+namespace Tyuiu.YarkovSD.Sprint1.Task7.V26.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidExpression()
         {
+            DataService ds = new DataService();
+            double x = 0;
+            double y = -7;
+            double wait = 155/102;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
         }
     }
 }
